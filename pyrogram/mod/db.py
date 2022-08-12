6f477f0fc180e41c9c9c7754fe58116b70fd0f04
Marcelo Batista <39688655+marcellobatiista@users.noreply.github.com>
2022-08-11 21:42:22 -0300

    async mongodb

--- pyrogram/mod/db.py
+++ pyrogram/mod/db.py
@@ -1,6 +1,5 @@
 import os
-from pymongo import MongoClient
-
+from motor.motor_asyncio import AsyncIOMotorClient
 
 class DataBase:
     def __init__(self, host=None):
@@ -9,29 +8,30 @@ class DataBase:
         else:
             self.host = host
 
-        self.mongodb = MongoClient(self.host)
+        self.mongodb = AsyncIOMotorClient(self.host)
         self.db = self.mongodb['Credenciais']
 
-    def busca(self, _id, key=None):
-        document = self.db.users.find_one({'_id': _id})
+    async def busca(self, _id, key=None):
+        document = await self.db.users.find_one({'_id': _id})
         if key:
             return document[key]
         return document
-    
-    def busca_by_referer(self, _id, key):
-        return self.db.users.find_one({'referer': _id})[key]
 
-    def atualiza(self, _id, key=None, value=None):
-        if type(key) == dict and not value:
-            self.db.users.update_one({'_id': _id}, {'$set': key})
-        else:
-            self.db.users.update_one({'_id': _id}, {'$set': {key: value}})
-            
-    def atualiza_by_referer(self, _id, key=None, value=None):
-        if type(key) == dict and not value:
-            self.db.users.update_one({'referer': _id}, {'$set': key})
-        else:
-            self.db.users.update_one({'referer': _id}, {'$set': {key: value}})
+    async def busca_by_referer(self, _id, key):
+        result = await self.db.users.find_one({'referer': _id})
+        return result[key]
 
-    def insere(self, dict):
-        self.db.users.insert_one(dict)
+    async def atualiza(self, _id, key=None, value=None):
+        if type(key) == dict and not value:
+            await self.db.users.update_one({'_id': _id}, {'$set': key})
+        else:
+            await self.db.users.update_one({'_id': _id}, {'$set': {key: value}})
+
+    async def atualiza_by_referer(self, _id, key=None, value=None):
+        if type(key) == dict and not value:
+            await self.db.users.update_one({'referer': _id}, {'$set': key})
+        else:
+            await self.db.users.update_one({'referer': _id}, {'$set': {key: value}})
+
+    async def insere(self, dict):
+        await self.db.users.insert_one(dict)
